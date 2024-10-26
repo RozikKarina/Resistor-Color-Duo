d2fb5c85bb84a8db8042f0995388ba90d07f76c8
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:39:49 +0300

    Добавьте файлы проекта.

---
+++ Resistor-Color-Duo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColorDuo
+{
+    private static readonly Dictionary<string, int> colorValues = new Dictionary<string, int>
+    {
+        { "black", 0 },
+        { "brown", 1 },
+        { "red", 2 },
+        { "orange", 3 },
+        { "yellow", 4 },
+        { "green", 5 },
+        { "blue", 6 },
+        { "violet", 7 },
+        { "grey", 8 },
+        { "white", 9 }
+    };
+
+    public static int Value(string[] colors)
+    {
+        if (colors.Length < 2)
+            throw new ArgumentException("At least two colors are required.");
+
+        // Get the values of the first two colors and concatenate them
+        int firstDigit = colorValues[colors[0].ToLower()];
+        int secondDigit = colorValues[colors[1].ToLower()];
+
+        // Combine the two values to form a two-digit number
+        return firstDigit * 10 + secondDigit;
+    }
+}
